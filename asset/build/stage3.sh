cca6855a49ea54a4b8a576baf3de1d0d3463601b
Paul G Webster <paul.g.webster@googlemail.com>
2022-07-03 01:45:45 +0100

    Updated and refined build system

--- asset/build/stage3.sh
+++ asset/build/stage3.sh
@@ -4,11 +4,11 @@
 # and then re-run stage2 to recompile ontop of the existing image
 # to ensure lib-linkage
 
-docker tag perlbase:stage2 perlbase:stage2backup \
+docker export perlbase:stage1 > stage1.tar \
 && docker rmi perlbase:stage1 \
 && docker tag perlbase:stage2 perlbase:stage1 \
-&& docker build -f Dockerfile.stage2 -t paulgwebster/perlbase:stage3 . \
-&& docker tag perlbase:stage2backup perlbase:stage2 \
-&& docker push paulgwebster/perlbase:stage3 || echo "Could not upload image"
+&& docker build -f Dockerfile.stage2 -t paulgwebster/perlbase:stage3 .
+
+docker push paulgwebster/perlbase:stage3 || echo "Could not upload image"
 
 echo "Relinked OS lib image created"
