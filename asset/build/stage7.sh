84405c93e3c56311475a56f4599a3ab5e13c946f
Paul G Webster <paul.g.webster@googlemail.com>
2022-07-03 01:27:20 +0100

    Restructured build stages to be more concise

--- asset/build/stage7.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -f Dockerfile.stage7 -t paulgwebster/perlbase:stage7 .         \
-&& docker tag paulgwebster/perlbase:stage7 perlbase:stage7                  \
-&& docker push paulgwebster/perlbase:stage7 || echo "Could not upload image"
-
-echo "Extended perl libs installed"
--- asset/build/stage8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -f Dockerfile.stage8 -t paulgwebster/perlbase:stage8 .         \
-&& docker tag paulgwebster/perlbase:stage8 perlbase:stage8                  \
-&& docker push paulgwebster/perlbase:stage8 || echo "Could not upload image"
-
-echo "Pre user level instance created"
